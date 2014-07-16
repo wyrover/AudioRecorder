e924526c8a7d9c20e4dda60114faeb03647f3a96
Ben Banerjee Richards <benjamin.mced@gmail.com>
2014-07-16 19:31:26 +0100

    Added README

--- AudioRecorder.cpp
+++ AudioRecorder.cpp
@@ -1,132 +1,140 @@
 #include "stdafx.h"
 #include "AudioRecorder.h"
 
-const int BUFFER_DURATION = 10;
+const int BUFFER_DURATION = 1;
 const int CHANNEL_COUNT = 1;
 const int SAMPLE_RATE = 44100;
-const int DATA_SIZE = SAMPLE_RATE * BUFFER_DURATION;
+const int DATA_SIZE = SAMPLE_RATE * BUFFER_DURATION * CHANNEL_COUNT;
 const int BUFFER_SIZE = 10;
 const int NUMBER_OF_BUFFERS = 5;
 
+WAVEHDR* hdr1;
 AudioRecorder::AudioRecorder()
 {
+	this->running = true;
 	buffers[NUMBER_OF_BUFFERS] = {};
+	SoundBuffer::Init(NUMBER_OF_BUFFERS);
 }
 
 
-bool AudioRecorder::Init()
+bool AudioRecorder::Init(void(*callback)(WAVEHDR data))
 {
-	/** Set up the format **/
+	this->callback = callback;
+
+	ZeroMemory(&format, sizeof(format));
 	format.nChannels = CHANNEL_COUNT;
 	format.cbSize = 0;
-	format.wFormatTag = WAVE_FORMAT_PCM;
 	format.nSamplesPerSec = SAMPLE_RATE;
-	format.wBitsPerSample = 8;	
-	format.nAvgBytesPerSec = SAMPLE_RATE * (format.wBitsPerSample / 8);
-	format.nBlockAlign = format.nChannels * format.wBitsPerSample / 8;
-
-	/** Open Device **/
-	 res = waveInOpen(&micHandle, WAVE_MAPPER, &format, (DWORD_PTR)waveOutCallback, (DWORD_PTR) this, CALLBACK_FUNCTION );
-	if (res != MMSYSERR_NOERROR){
-		std::cerr << "waveInOpen() Failed " << res << std::endl;
-		return false;
-	}
-
-	WAVEHDR buffer;
-	short int data[DATA_SIZE];
-
-	buffer.lpData = (LPSTR)data;
-	buffer.dwBufferLength = DATA_SIZE;
-	buffer.dwBytesRecorded = 0;
-	buffer.dwUser = 0;
-	buffer.dwFlags = 0;
-	buffer.dwLoops = 0;
-
-	/***
-	res = waveInPrepareHeader(micHandle, &buffer, sizeof(WAVEHDR));
-	if (res != MMSYSERR_NOERROR)
-	{
-		std::cerr << "waveInPrepareHeader() Failed " << res << std::endl;
-		return false;
-	}
-
-	res = waveInAddBuffer(micHandle, &buffer, sizeof(WAVEHDR));
-	if (res != MMSYSERR_NOERROR)
-	{
-		std::cerr << "waveInAddBuffer() Failed " << res << std::endl;
-		return false;
-	} ***/
+	format.wFormatTag = WAVE_FORMAT_PCM;
+	format.wBitsPerSample = 8;
+	format.nBlockAlign = (CHANNEL_COUNT * format.wBitsPerSample) / 8;
+	format.nAvgBytesPerSec = SAMPLE_RATE * format.nBlockAlign;
 	
-	addBuffer(buffer);
-	/** Crash happens here ***/
-	res = waveInStart(micHandle);
+	res = waveInOpen(&micHandle, WAVE_MAPPER, &format, (DWORD_PTR)waveInCallback, 0, CALLBACK_FUNCTION );
 	if (res != MMSYSERR_NOERROR)
 	{
-		std::cerr << "waveInStart() Failed " << res << std::endl;
+		std::cerr << "waveInOpen() failed" << std::endl;
+		return false;
 	}
-	
-	Sleep(10000);
+
+	for (int i = 0; i < NUMBER_OF_BUFFERS; i++)
+	{
+		WAVEHDR header;
+		ZeroMemory(&header, sizeof(header));
+		
+
+		header.lpData = (LPSTR)new short int[DATA_SIZE];
+		header.dwBufferLength = DATA_SIZE;
+		header.dwBytesRecorded = 0;
+		header.dwUser = 0;
+		header.dwFlags = 0;
+		header.dwLoops = 0;
+		header.dwUser = i;	
+
+		buffers[i] = header;
+		SoundBuffer::SetAwaitingCompletion(i); 
+		addBuffer(&buffers[i]);
+	}
 	
 	return true;
 }
 
-bool AudioRecorder::addBuffer(WAVEHDR buffer)
+bool AudioRecorder::addBuffer(WAVEHDR* buffer)
 {
-	 res = waveInPrepareHeader(micHandle, &buffer, sizeof(WAVEHDR));
+	res = waveInPrepareHeader(micHandle, buffer, sizeof(WAVEHDR));
 	if (res != MMSYSERR_NOERROR)
 	{
-		std::cerr << "waveInPrepareHeader() Failed " << res << std::endl;
-		return false;
-	}
-	
-	res = waveInAddBuffer(micHandle, &buffer, sizeof(WAVEHDR));
-	if (res != MMSYSERR_NOERROR)
-	{
-		std::cerr << "waveInAddBuffer() Failed " << res << std::endl;
+		std::cerr << "waveInPrepareHeader() failed" << std::endl;
 		return false;
 	}
 
+	res = waveInAddBuffer(micHandle, buffer, sizeof(WAVEHDR));
+	if (res != MMSYSERR_NOERROR)
+	{
+		std::cerr << "waveInAddBuffer() failed" << std::endl;
+		return false;
+	}
 	return true;
 }
 
+void AudioRecorder::Update()
+{
+	std::vector<int> ids = SoundBuffer::GetUnpreparedIDs();
+	
+	for each (int id in ids)
+	{
+		callback(buffers[id]);
 
-void CALLBACK waveOutCallback(HWAVEIN hWaveOut, UINT uMesg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
+		buffers[id].dwUser = id;
+		addBuffer(&buffers[id]);
+
+		SoundBuffer::SetAwaitingCompletion(id);
+	}
+
+}
+
+WAVEHDR AudioRecorder::getNewWAVEHDR()
+{
+	short int data[DATA_SIZE];
+	WAVEHDR header;
+	ZeroMemory(&header, sizeof(header));
+
+	header.lpData = (LPSTR)&data;
+	header.dwBufferLength = DATA_SIZE;
+	header.dwBytesRecorded = 0;
+	header.dwUser = 0;
+	header.dwFlags = 0;
+	header.dwLoops = 0;
+
+	return header;
+}
+
+void CALLBACK waveInCallback(HWAVEIN hWaveOut, UINT uMesg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
 {
 	if (uMesg == WIM_DATA)
 	{
-		std::cout << "complete" << std::endl;
+		WAVEHDR* hdr = (WAVEHDR*)dwParam1;
+		int id = hdr->dwUser;
+
+		SoundBuffer::SetUnprepared(id);
 	}
 }
-
-
 
 bool AudioRecorder::StartRecording()
 {
-	
+	res = waveInStart(micHandle);
+	if (res != MMSYSERR_NOERROR)
+	{
+		std::cerr << "waveInStart() failed" << std::endl;
+		return false;
+	}
+
 	return true;
 }
 
-
-
-void AudioRecorder::HandleError(std::string additional_info)
+void AudioRecorder::StopRecording()
 {
-	int error_code = GetLastError();
-	LPSTR buffer = NULL;
-	int size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&buffer, 0, NULL);
-
-	std::string message;
-	if (additional_info != "")
-	{
-		message = "[" + additional_info + "] " + buffer;
-	}
-	else {
-		message = buffer;
-	}
-
-	std::cerr << message << std::endl;
-
-	LocalFree(buffer);
+	waveInStop(micHandle);
 }
 
 AudioRecorder::~AudioRecorder()
@@ -134,35 +142,3 @@ AudioRecorder::~AudioRecorder()
 }
 
 
-void playData(WAVEHDR hdr)
-{
-	WAVEFORMATEX format;
-	/** Set up the format **/
-	format.nChannels = CHANNEL_COUNT;
-	format.cbSize = 0;
-	format.wFormatTag = WAVE_FORMAT_PCM;
-	format.nSamplesPerSec = SAMPLE_RATE;
-	format.wBitsPerSample = 8;
-	format.nAvgBytesPerSec = SAMPLE_RATE * (format.wBitsPerSample / 8);
-	format.nBlockAlign = format.nChannels * format.wBitsPerSample / 8;
-
-	HWAVEOUT waveOut;
-
-
-	MMRESULT res = waveOutOpen(&waveOut, WAVE_MAPPER, &format, 0, 0, CALLBACK_NULL);
-
-	if (res != MMSYSERR_NOERROR)
-	{
-		std::cout << "Failed" << std::endl;
-	}
-
-	waveOutPrepareHeader(waveOut, &hdr, sizeof(WAVEHDR));
-
-	waveOutWrite(waveOut, &hdr, sizeof(WAVEHDR));
-
-	Sleep(500);
-	while (waveOutUnprepareHeader(waveOut, &hdr, sizeof(WAVEHDR)) == WAVERR_STILLPLAYING)
-	{
-	}
-
-}
--- AudioRecorder.h
+++ AudioRecorder.h
@@ -1,31 +1,31 @@
 #pragma once
-#include "CircularBuffer.h"
 
 class AudioRecorder
 {
 public:
 	AudioRecorder();
 	~AudioRecorder();
+	void Update();
 
-	bool Init();
-	bool packetAvailiable();
+	bool Init(void(*callback)(WAVEHDR data));
 
 	bool StartRecording();
 	void StopRecording();
 
-
-	WAVEHDR getLatestPacket();
+	WAVEHDR getNewWAVEHDR();
 
 private:
+	void(*callback)(WAVEHDR data);
+	bool running;  
+
 	MMRESULT res;
 	HWAVEIN micHandle;
 	WAVEFORMATEX format;
 	WAVEHDR buffers[];
 
-	void HandleError(std::string additional_info);
-	bool addBuffer(WAVEHDR buffer);
+	bool addBuffer(WAVEHDR *buffer);
 };
 
-static void CALLBACK waveOutCallback(HWAVEIN hWaveOut, UINT uMesg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
+static void CALLBACK waveInCallback(HWAVEIN hWaveOut, UINT uMesg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
 void playData(WAVEHDR hdr);
 
